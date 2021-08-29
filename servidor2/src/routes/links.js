6cdf4185b35b78d21283c52adcd9d6719e2b95ca
Jose-Alcivar-C <josealci@espol.edu.ec>
2021-08-29 17:52:36 -0500

    version04

--- servidor2/src/routes/links.js
+++ servidor2/src/routes/links.js
@@ -4,11 +4,14 @@ const router = express.Router();
 
 const pool = require("../database");
 
-router.get("/mascotas/add", (req, res) => {
-    res.render("links/add");
+//---------------------------------------------------PARA LAS MASCOTAS----------------------------------------------------------------
+//ventana para añadir mascota
+router.get("/mascotas/anadir", (req, res) => {
+    res.render("links/anadirmascota");
 });
 
-router.post("/mascotas/add", async (req, res) => {
+//añadir mascota
+router.post("/mascotas/anadir", async (req, res) => {
 
     const{ nombre, edad, sexo, descripcion, url_foto, ciudad, raza } = req.body;
 
@@ -27,11 +30,44 @@ router.post("/mascotas/add", async (req, res) => {
     res.redirect("/links/mascotas");
 });
 
+//ventana para mostrar mascotas
 router.get("/mascotas", async (req, res) => {
     
     const mascotas = await pool.query("select * from mascota");
-    console.log(mascotas);
     res.render("links/mascotas", {mascotas: mascotas});
 });
 
+//borrar una mascota
+router.get("/mascotas/borrar/:id", async (req, res) =>{
+    const { id } = req.params;
+    await pool.query("delete from mascota where id_mascota = ?", [id]);
+    res.redirect("/links/mascotas");
+});
+
+//ventana para editar una mascota
+router.get("/mascotas/editar/:id", async (req, res) =>{
+    const { id } = req.params;
+    const mascota = await pool.query("select * from mascota where id_mascota =?", [id]);
+    res.render("links/editarmascota", { mascota:mascota[0] });
+});
+
+//editar mascota
+router.post("/mascotas/editar/:id", async (req, res) =>{
+    const { id } = req.params;
+    const { nombre, edad, sexo, descripcion, url_foto, ciudad, raza } = req.body;
+    const nuevosDatos = {
+        nombre, 
+        edad, 
+        sexo, 
+        descripcion, 
+        url_foto, 
+        ciudad, 
+        raza 
+    };
+
+    await pool.query("update mascota set ? where id_mascota = ?", [nuevosDatos, id]);
+    res.redirect("/links/mascotas");
+});
+
+//---------------------------------------------------PARA LAS NOTICIAS----------------------------------------------------------------
 module.exports = router;
